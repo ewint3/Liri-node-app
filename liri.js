536048ed3df7ed9ca65bf7eb74ecf998dd81cdcf
Weezer <ewint3@gmail.com>
2019-12-18 19:29:43 -0600

    trying to get it to work with no go

--- liri.js
+++ liri.js
@@ -1,122 +1,142 @@
-// DEPENDENCIES
-
 require("dotenv").config();
+var keys = require("./keys")
+var Spotify = require("node-spotify-api")
+var dateFormat = require("dateFormat")
+var fs = require("fs")
 
-var Spotify = require("node-spotify-api");
-// Import the API keys
-var keys = require("./keys");
-// Import the request npm package.
-var request = require("request");
-// Import the FS package for read/write.
-var fs = require("fs");
-// Initialize the spotify API client with keys
-var spotify = new Spotify(keys.spotify);
+// Takes an artist and searches the Bands in Town 
+// Artist API for an artist and render information
+var concertThis = function(artist){
+    var region = ""
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist.replace(" ", "+") + "/events?app_id=codingbootcamp"
+    //console.log(queryUrl);
+    
+    request(queryUrl, function(err, response, body){
+        // If the request is successful
+        if (!err && response.statusCode === 200) {
+            // Save parsed body in a new variable for easier use
+            var concertInfo = JSON.parse(body)
+            
+            outputData(artist + " concert information:")
 
-// Function for running a Spotify search
-var callSpotifyAPI = function(songName) {
-  if (songName === undefined) {
-    songName = "1999";
-  }
-  spotify.search(
-    {
-      type: "track",
-      query: songName,
-      limit: 15
-    },
-    function(err, data) {
-      if (err) {
-        console.log("Error occurred: " + err);
-        return;
-      }
-      var songs = data.tracks.items;
-      for (var i = 0; i < songs.length; i++) {
-        console.log(i);
-        console.log("Artist name: " + songs[i].artists[0].name);
-        console.log("Song title: " + songs[i].name);
-        console.log("Track number: " + songs[i].track_number);
-        console.log("Album: " + songs[i].album.name);
-        console.log("Release date: " + songs[i].album.release_date);
-        console.log("Album type: " + songs[i].album.album_type);
-        console.log("Preview song: " + songs[i].preview_url);
-        console.log("----------------------------------------------------");
-      }
-    }
-  );
-};
-// =====================================
-// Function for running a OMDB Search
-// _____________________________________
-var callOMDBAPI = function(movieName) {
-  if (movieName === undefined) {
-    movieName = "snakes on a plane";
-  }
-  var urlHit =
-  "http://www.omdbapi.com/?t=" +
-  movieName +
-  "&y=&plot=full&tomatoes=true&apikey=trilogy";
-  request(urlHit, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var jsonData = JSON.parse(body);
-      console.log("Title: " + jsonData.Title);
-      console.log("Year: " + jsonData.Year);
-      console.log("Rated: " + jsonData.Rated);
-      console.log("IMDB Rating: " + jsonData.imdbRating);
-      console.log("Country: " + jsonData.Country);
-      console.log("Language: " + jsonData.Language);
-      console.log("Plot: " + jsonData.Plot);
-      console.log("Actors: " + jsonData.Actors);
-      console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
-    }
-  });
-};
-// =====================================
-// Function for determining which command is executed
-// _____________________________________
-var userCommand = function(caseData, functionData) {
-  switch (caseData) {
-    // use twitter api
-    case "my-tweets":
-    callTwitterAPI();
-    break;
-    // use spotify api
-    case "spotify-this-song":
-    callSpotifyAPI(functionData);
-    break;
-    // use omdb api
-    case "movie-this":
-    callOMDBAPI(functionData);
-    break;
+            for (i=0; i < concertInfo.length; i++) {
+                
+                region = concertInfo[i].venue.region
+                 //handle Canadian venues
+                if (region === "") {
+                    region = concertInfo[i].venue.country
+                }
 
-    case "do-what-it-says":
-    doWhatItSays();
-    break;
-    default:
-    console.log("LIRI can't understand your nonsense!");
-  }
-};
-// =====================================
-// Function to take data from .txt file and send to another function when user enters "do-what-it-says"
-// _____________________________________
-var doWhatItSays = function () {
-  fs.readFile('random.txt', 'utf8', function(error, data){
-    console.log(data);
-    var dataArr = data.split(',');
-    if (dataArr.length ===  2) {
-      userCommand(dataArr[0], dataArr[1]);
-    } else if (dataArr.length === 1) {
-      userCommand(dataArr[0]);
+                // Need to return Name of venue, Venue location, Date of event (MM/DD/YYYY)
+                outputData("Venue: " + concertInfo[i].venue.name)
+                outputData("Location: " + concertInfo[i].venue.city + ", " + region);
+                outputData("Date: " + dateFormat(concertInfo[i].datetime, "mm/dd/yyyy"))
+            }
+        }
+    })
+}
+
+// This will take a song, search spotify and return information
+var spotifyThisSong = function(song){
+    // Default should be "The Sign" by Ace of Base
+    if (!song){
+        song = "The Sign Ace of Base"
     }
-  });
-};
-// =====================================
-// Function which takes in command line arguments and executes switch statement accordigly
-// _____________________________________
-var cmdLnArgs = function(argOne, argTwo) {
-  userCommand(argOne, argTwo);
-};
-// =====================================
-// this takes in user input and assigns them as arguments
-// _____________________________________
-cmdLnArgs(process.argv[2], process.argv[3]);
+
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({type: "track", query: song, limit: 1}, function (err, data){
+        if (err) {
+            return console.log(err)
+        }
+
+        // Need to return Artist(s), Song Name, Album, Preview link of song from Spotify
+        var songInfo = data.tracks.items[0]
+        outputData(songInfo.artists[0].name)
+        outputData(songInfo.name)
+        outputData(songInfo.album.name)
+        outputData(songInfo.preview_url)
+    })
+}
+
+// This will take a movie, search IMDb and return information
+var movieThis = function(movie){
+    // Default should be "Mr. Nobody"
+    if (!movie){
+        movie = "Mr.+Nobody"
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    //console.log(queryUrl);
+
+    // Then create a request to the queryUrl
+    request(queryUrl, function(err, response, body){
+        // If the request is successful
+        if (!err && response.statusCode === 200) {
+            // Need to return: Title, Year, IMDB Rating, Rotten Tomatoes Rating, Country, 
+            // Language, Plot, Actors
+            var movieInfo = JSON.parse(body)
+
+            outputData("Title: " + movieInfo.Title)
+            outputData("Release year: " + movieInfo.Year)
+            outputData("IMDB Rating: " + movieInfo.imdbRating)
+            outputData("Rotten Tomatoes Rating: " + movieInfo.Ratings[1].Value)
+            outputData("Country: " + movieInfo.Country)
+            outputData("Language: " + movieInfo.Language)
+            outputData("Plot: " + movieInfo.Plot)
+            outputData("Actors: " + movieInfo.Actors)
+        }
+    })
+}
+
+// Using the `fs` Node package, LIRI will take the text inside of random.txt
+// and then use it to call one of LIRI's commands.
+var doWhatItSays = function(){
+
+    // read from file
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if(err){
+            return console.log(err)
+        }
+        
+        var dataArr = data.split(",")
+
+        // call appropriate function and pass arguement
+        runAction(dataArr[0], dataArr[1])
+    });
+}
+
+// This function will handle outputting to the console and writing to log file
+var outputData = function(data) {
+    console.log(data)
+
+    fs.appendFile("log.txt", "\r\n" + data, function (err){
+        if(err){
+            return console.log(err)
+        } 
+    })
+}
+
+var runAction = function(func, parm) {
+    switch (func) {
+        case "concert-this":
+            concertThis(parm)
+            break
+        case "spotify-this-song":
+            spotifyThisSong(parm)
+            break
+        case "movie-this":
+            movieThis(parm)
+            break
+        case "do-what-it-says":
+            doWhatItSays()
+            break
+        default:
+            outputData("That is not a command that I recognize, please try again.") 
+    }
+}
+
+runAction(process.argv[2], process.argv[3])   
+
 
 
